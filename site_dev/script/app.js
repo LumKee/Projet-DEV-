d8e991174eac9fee142e462028d2d457323e3eb2
RaphaelRT <raph.robert974@gmail.com>
2018-06-17 17:45:39 +0200

    fix

--- site_dev/script/app.js
+++ site_dev/script/app.js
@@ -16,3 +16,15 @@ $(".diamond-grid").diamonds({
     autoRedraw : true,
     itemSelector :".item"
 });
+
+function initMap() {
+        var uluru = {lat: 48.859252 , lng: 2.431387};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
