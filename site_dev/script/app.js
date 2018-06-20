de674e9c6754e8af47e602c88923a41f555f9b79
RaphaelRT <raph.robert974@gmail.com>
2018-06-21 00:08:24 +0200

    fix

--- site_dev/script/app.js
+++ site_dev/script/app.js
@@ -1,10 +1,11 @@
-var SlideArrow= document.querySelectorAll(".ScrollArrow")
-var SlideArrowWidth= document.querySelectorAll(".ScrollArrow").offsetHeight
-let firstPic= document.querySelectorAll("backgroundImg").offsetHeight
-function Slidearrow(){
-  let position = SlideArrowWidth-firstPic
-  SlideArrow.style.top="width"
-}
+$(document).ready(function() {
+    $('.js-scrollTo').on('click', function() {
+        var page = $(this).attr('href');
+        var speed = 750;
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+        return false;
+    });
+});
 
 
 $('.slider').glide({
@@ -17,21 +18,24 @@ $('.slider').glide({
 // Diamond gallery
 
 $(".diamond-grid").diamonds({
-	  size : 150,
-    gap : 5,
-   	hideIncompleteRow : false,
-    autoRedraw : true,
-    itemSelector :".item"
+  size: 100,
+  gap: 5,
+  hideIncompleteRow: false,
+  autoRedraw: true,
+  itemSelector: ".item"
 });
 
 function initMap() {
-        var uluru = {lat: 48.859252 , lng: 2.431387};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+  var uluru = {
+    lat: 48.859252,
+    lng: 2.431387
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
